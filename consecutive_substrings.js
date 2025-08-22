4dfc622481326b34a3f1c66d90d697c305ae474c
phillsatellite <pprints12@gmail.com>
2025-08-22 19:52:49 -0400

    SubStrings

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,25 @@
-function consecutiveSubstrings(string) {
-  // type your code here
+function consecutiveSubstrings(str) {
+  //Create a place to store the results
+  const result = [];
+  
+  //Iterate through each character in the text
+  for (let start = 0; start < str.length; start++) {
+    //Take the substring from the current start to end and add it to the results
+    for (let end = start + 1; end <= str.length; end++) {
+      result.push(str.slice(start, end));
+    }
+  }
+  return result;
 }
 
+
 if (require.main === module) {
-  // add your own tests in here
+  console.log("Expecting: ['m', 'mn', 'mno', 'n', 'no', 'o']");
+  console.log("")
+  
+  console.log("Expecting: ['x', 'xy', 'xyz', 'y', 'yz', 'z']");
+  console.log("=>", consecutiveSubstrings(`xyz`));
+
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
